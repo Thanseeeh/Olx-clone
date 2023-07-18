e119bae5ca915fa0a8102ae9898c92e7829deb71
Thanseeh yaseen <yaseenkdr27@gmail.com>
2023-07-18 17:58:17 +0530

    routing and add products

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
-
+import React, { useState, useContext } from 'react';
+import { FirebaseContext } from '../../store/Context';
 import Logo from '../../olx-logo.png';
 import './Login.css';
+import {useHistory} from 'react-router-dom'
 
 function Login() {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const {firebase} = useContext(FirebaseContext)
+  const history = useHistory()
   const handleLogin = (e) => {
     e.preventDefault()
+    firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
+      history.push('/')
+    }).catch((error) => {
+      alert(error.message)
+    })
   }
   return (
     <div>
